c0247103dd1319a3b9a9277e09fb7b61e0b2069e
hassan-nahid <scientisthassannahid@gmail.com>
2025-07-10 23:27:32 +0600

    part-1 finised

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
-
+import cors from "cors"
 import express, { Request, Response } from 'express';
+import { router } from "./app/routes";
+import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
+import notFound from "./app/middleware/notFound";
 
+const app = express();
+app.use(express.json());
+app.use(cors())
 
+app.use("/api/v1", router)
 
-
-const app = express()
-
-app.get("/",(req:Request, res:Response)  => {
+app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
-        message: "Welcome to the tour management system" 
+        message: "Welcome to the tour management system"
     })
 })
+
+app.use(globalErrorHandler)
+app.use(notFound)
 
 export default app
