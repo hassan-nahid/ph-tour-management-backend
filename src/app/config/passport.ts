ab21291c32a66700d6888208810867d96409296f
hassan-nahid <scientisthassannahid@gmail.com>
2025-07-16 15:26:59 +0600

    part-4 completed

--- src/app/config/passport.ts
+++ src/app/config/passport.ts
@@ -4,6 +4,38 @@ import { Strategy as GoogleStrategy, Profile, VerifyCallback } from "passport-go
 import { envVars } from "./env";
 import { User } from "../modules/user/user.model";
 import { Role } from "../modules/user/user.interface";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcryptjs from "bcryptjs"
+
+passport.use(
+    new LocalStrategy({
+        usernameField: "email",
+        passwordField: "password"
+    }, async (email: string, password: string, done) => {
+        try {
+            const isUserExist = await User.findOne({ email })
+            if (!isUserExist) {
+                return done(null, false, { message: "User does not exist" })
+            }
+         
+
+            const isGoogleAuthenticated = isUserExist.auths.some(providerObjects => providerObjects.provider === "google")
+            if (isGoogleAuthenticated && !isUserExist.password) {
+                return done(null, false, { message: "You have authenticated through Google. So if you want to login with credentials, then at first login with Google and set a password for your Gmail and then you can login with email and password." })
+            }
+            
+
+            const isPasswordMatched = await bcryptjs.compare(password as string, isUserExist.password as string)
+            if (!isPasswordMatched) {
+                return done(null, false, { message: "Password does not match" })
+            }
+            return done(null, isUserExist)
+
+        } catch (error) {
+            done(error)
+        }
+    })
+)
 
 passport.use(
     new GoogleStrategy(
@@ -12,14 +44,14 @@ passport.use(
             clientSecret: envVars.GOOGLE_CLIENT_SECRET,
             callbackURL: envVars.GOOGLE_CALLBACK_URL
         }, async (accessToken: string, refreshToken: string, profile: Profile, done: VerifyCallback) => {
-            try{
+            try {
                 const email = profile.emails?.[0].value
-                if(!email){
-                    return done(null, false, {message: "No email found"})
+                if (!email) {
+                    return done(null, false, { message: "No email found" })
                 }
 
-                let user = await User.findOne({email})
-                if(!user){
+                let user = await User.findOne({ email })
+                if (!user) {
                     user = await User.create({
                         email,
                         name: profile.displayName,
@@ -35,21 +67,21 @@ passport.use(
                     })
                 }
                 return done(null, user)
-            }catch(error){
+            } catch (error) {
                 return done(error)
-            }   
+            }
         }
     )
 )
 
-passport.serializeUser((user: any, done:(err: any, id?: unknown) => void) => 
-done(null,user._id))
+passport.serializeUser((user: any, done: (err: any, id?: unknown) => void) =>
+    done(null, user._id))
 
-passport.deserializeUser( async (id: string, done: any) => {
-    try{
+passport.deserializeUser(async (id: string, done: any) => {
+    try {
         const user = await User.findById(id)
         done(null, user)
-    }catch(error){
+    } catch (error) {
         done(error)
-    }   
+    }
 })
